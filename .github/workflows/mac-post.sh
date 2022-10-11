664d8627d13a451f75cb154839805d71148aced9
Cryptiiiic <liamwqs@gmail.com>
2022-10-11 16:26:14 -0700

    Add Versioning.zip

--- .github/workflows/mac-post.sh
+++ .github/workflows/mac-post.sh
@@ -7,6 +7,9 @@ export BASE=/Users/runner/work/iBoot64Patcher/iBoot64Patcher/
 
 cd ${BASE}
 export iBoot64Patcher_VERSION=$(git rev-list --count HEAD | tr -d '\n')
+export iBoot64Patcher_VERSION_SHA=$(git rev-parse HEAD | tr -d '\n')
+echo -n $iBoot64Patcher_VERSION_SHA > latest_build_sha.txt
+echo -n $iBoot64Patcher_VERSION > latest_build_num.txt
 tar cpPJf "iBoot64Patcher-macOS-x86_64-Build_${iBoot64Patcher_VERSION}-RELEASE.tar.xz" -C cmake-build-release-x86_64 iBoot64Patcher
 tar cpPJf "iBoot64Patcher-macOS-x86_64-Build_${iBoot64Patcher_VERSION}-DEBUG.tar.xz" -C cmake-build-debug-x86_64 iBoot64Patcher
 tar cpPJf "iBoot64Patcher-macOS-arm64-Build_${iBoot64Patcher_VERSION}-RELEASE.tar.xz" -C cmake-build-release-arm64 iBoot64Patcher
